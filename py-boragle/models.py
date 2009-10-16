b5f0ca3a339b7d2e4a514daf4d66cf0e09a55efc
Sudhir Jonathan <sudhir.j@gmail.com>
2009-10-16 18:49:03 +0530

    built up the domain model

--- py-boragle/models.py
+++ py-boragle/models.py
@@ -1,9 +1,47 @@
 from google.appengine.ext import db
 
-def validate_slugs(slugs):
-    if len(slugs) < 1: raise db.BadValueError
-
-class Boragle(db.Model):
-    name = db.StringProperty(required = True)
-    slugs = db.StringListProperty(validator = validate_slugs)
+class ExtendedModel(db.Model):
+    created_at = db.DateTimeProperty(auto_now_add = True)
+    updated_at = db.DateTimeProperty(auto_now = True)
     
+    @classmethod
+    def _find_by(cls, key, value):
+        matches = cls.all().filter(key+' =', value).fetch(1)
+        return matches[0] if len(matches) else None
+
+class HasSlugs:
+    @staticmethod
+    def validate_slugs(slugs):
+        if not len(slugs): raise db.BadValueError
+
+    @classmethod
+    def find_by_slug(cls, slug):
+        return cls._find_by('slugs', slug)
+
+class HasComments:
+    pass
+
+class HasVotes:
+    pass
+    
+class Boragle(ExtendedModel, HasSlugs):
+    name = db.StringProperty(required = True)
+    desc = db.TextProperty()
+    slugs = db.StringListProperty(validator = HasSlugs.validate_slugs)
+
+class CommentableModel(ExtendedModel, HasComments, HasVotes):
+    votes = db.IntegerProperty()    
+
+class Comment(ExtendedModel):
+    text = db.TextProperty()
+    owner = db.ReferenceProperty(CommentableModel,collection_name='comments')
+    
+class Question(CommentableModel, HasSlugs):
+    boragle = db.ReferenceProperty(Boragle,collection_name='questions')
+    slugs = db.StringListProperty(validator = HasSlugs.validate_slugs)
+    
+class Answer(CommentableModel):
+    question = db.ReferenceProperty(Question,collection_name='answers')
+
+
+ 
--- py-boragle/tests/boragle_tests.py
+++ py-boragle/tests/boragle_tests.py
@@ -8,11 +8,15 @@ class BoragleTest(base.ExtendedTestCase):
         koi_boragle = Boragle(name = "Koi", desc= "Boragle about koi fish", slugs = ["koi"])
         koi_boragle.put()
         
-        boragle = Boragle.all().fetch(1)[0]
+        boragle = Boragle.find_by_slug('koi')
         self.assertEqual(koi_boragle.name,boragle.name)
+        self.assertEqual(koi_boragle.desc,boragle.desc)
         
     def test_slug_list_validator(self):
         self.assertRaises(db.BadValueError,Boragle,name = "Koi", desc = "some desc", slugs = [])
+    
+    def test_finder_returns_none_no_match(self):
+        self.assertEqual(None,Boragle.find_by_slug('blah'))
         
     
         
